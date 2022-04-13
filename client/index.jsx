71588fb71d184479708282e9e8c4072813199b65
tiredhag <killboi.artgmail.com>
2022-04-14 00:01:16 +0200

    serve react from express

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
-ReactDOM.render(<h1> Hello World</h1>, document.getElementById("app"));
+function FrontPage() {
+  return (
+    <div>
+      <h1>Movie App</h1>
+      <div>
+        <div>
+          <Link to={"/login"}>Login</Link>
+        </div>
+        <br />
+        <div>
+          <Link to={"/register"}>Register new user</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Login() {
+  return <h1>Hello you dumbfuck, this is the login page</h1>;
+}
+
+function Application(props) {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<FrontPage />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
--- server/server.js
+++ server/server.js
@@ -5,8 +5,17 @@ const app = express();
 app.get("/api/login", (req, res) => {
   res.json({
     username: "admin",
-    fullName: "A Person Sorta",
+    fullName: "Shreks Bussy",
   });
+});
+
+app.use(express.static("../client/dist"));
+app.use((req, res, next) => {
+  if (req.method == "GET" && !req.path.startsWith("/api/")) {
+    return res.sendFile(path.resolve("/client/dist/index.html"));
+  } else {
+    next();
+  }
 });
 
 const server = app.listen(process.env.PORT || 3000, () => {
