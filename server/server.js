ec2b1d19f9e9644c4b78a6bcf185bb960ca7a85a
tiredhag <killboi.artgmail.com>
2022-04-13 21:14:02 +0200

    dummy getend point in express

--- server/server.js
+++ server/server.js
@@ -2,6 +2,13 @@ import express from "express";
 
 const app = express();
 
+app.get("/api/login", (req, res) => {
+  res.json({
+    username: "admin",
+    fullName: "A Person Sorta",
+  });
+});
+
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Server started on http://localhost:${server.address().port}`);
 });
